95dd36d146efa406cd670de4f78a1f0a832a5c34
Iván Vilches Basaul <ivan_vilches@hotmail.com>
2015-11-19 07:35:58 +0000

    added root route pointing to uploads and new action, also added uploads resources which provide a nice restful urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'uploads/new'
+  root 'uploads#new'
+  resources :uploads
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
